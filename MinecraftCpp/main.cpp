19d52403253c8fe8e121bfb35890870ff595261f
Florian Kaiser <mail@floriankaiser.org>
2014-10-04 17:09:27 +0200

    Initial Commit

---
+++ MinecraftCpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MinecraftCpp
+//
+//  Created by florian on 04.10.14.
+//  Copyright (c) 2014 florian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
